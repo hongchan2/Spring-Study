4efda1d4465dfc4f1dc98cf15a1953cb5246b212
hongchan2 <ipeter@naver.com>
2020-03-06 23:08:44 +0900

    자동 설정 (AutoConfiguration)

---
+++ Spring Framework/Spring Boot/1. 자동 설정/hongchansprringbootstarter/src/main/java/kr/ac/kw/Holoman.java
@@ -0,0 +1,32 @@
+package kr.ac.kw;
+
+public class Holoman {
+
+    String name;
+
+    int howLong;
+
+    public int getHowLong() {
+        return howLong;
+    }
+
+    public void setHowLong(int howLong) {
+        this.howLong = howLong;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Holoman{" +
+                "name='" + name + '\'' +
+                ", howLong=" + howLong +
+                '}';
+    }
+}
---
+++ Spring Framework/Spring Boot/1. 자동 설정/springbootsecond/src/main/java/kr/ac/kw/Application.java
@@ -0,0 +1,17 @@
+package kr.ac.kw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.setWebApplicationType(WebApplicationType.NONE);
+        springApplication.run(args);
+    }
+
+}
