1c3fada50e50962dc524b8707ffb11da61b8467c
hongchan2 <ipeter@naver.com>
2020-05-06 15:05:34 +0900

    EntityGraph

---
+++ Spring Data JPA/6. 스프링 데이터 JPA/spring-data-jpa/src/test/java/com/hongchan/springdatajpa/Post/CommentRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.hongchan.springdatajpa.Post;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CommentRepositoryTest {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Test
+    public void doTest() {
+        commentRepository.getById(1l);
+
+        System.out.println("=========");
+
+        commentRepository.findById(1l);
+    }
+
+}
